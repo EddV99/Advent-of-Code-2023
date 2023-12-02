6d7ba07352291cb11d1f9b9144dcd8a4b69c44fe
Eduardo Valdivia <eddval0808@gmail.com>
2023-12-02 15:23:43 -0700

    template of main and makefile

---
+++ template/main_template.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+std::unique_ptr<std::ifstream> openFile(std::string filePath) {
+  std::unique_ptr<std::ifstream> file =
+      std::make_unique<std::ifstream>(filePath);
+  if (!file->is_open()) {
+    std::cout << "Coudn't find input file\n";
+    exit(1);
+  }
+  return file;
+}
+
+
+int main(int argc, char **argv) {
+  std::string inputFile;
+  if (argc > 1)
+    inputFile = std::string(argv[1]);
+  else {
+    std::cout << "No input file given\n";
+  }
+
+  return 0;
+};
